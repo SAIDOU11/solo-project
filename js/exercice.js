4fe5c5f3a798649ef2f54fa7a2e79d4f85134088
Salif Saidou <saidou02100@gmail.com>
2023-02-18 20:17:28 +0100

    Blackjack Game scrimba project

---
+++ js/exercice.js
@@ -0,0 +1,10 @@
+let castle = {
+  title: "Castleroc",
+  rooms: 16,
+  owner: "Per Harald Borgen",
+  isFree: false,
+  sites: ["London", "Manchester"],
+};
+
+console.log(castle.sites);
+console.log(castle.title);
--- js/index.js
+++ js/index.js
@@ -1,27 +1,70 @@
-console.log(4 === 3); // false
-console.log(5 > 2); // true
-console.log(12 > 12); // false
-console.log(3 < 0); // false
-console.log(3 >= 3); // true
-console.log(11 <= 11); // true
-console.log(3 <= 2); // false
+let cards = []; // array - ordered list of items
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+// 2. Create the player object. Give it two keys, name and chips, and set their values
+let player = {
+  name: "Saidou",
+  chips: 145,
+};
 
-// let firstCard = 10;
-// let secondCard = 15;
-// let sum = firstCard + secondCard;
-// let hasBlackJack = false;
-// let isAlive = true;
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": " + player.chips + " $";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
+console.log(cards);
 
-// if (sum <= 20) {
-//   console.log("Do you want to draw a new card? 🙂");
-// } else if (sum === 21) {
-//   console.log("Wohoo! You've got Blackjack! 🥳");
-//   hasBlackJack = true;
-// } else {
-//   console.log("You're out of the game! 😭");
-//   isAlive = false;
-// }
+// 1. Create a function, getRandomCard(), that always returns the number 5
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
+console.log(getRandomCard());
 
-// // CASH OUT!
+function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards.push(firstCard, secondCard);
+  sum = firstCard + secondCard;
+  getRandomCard();
+  renderGame();
+  console.log(cards);
+}
 
-// console.log(isAlive);
+function renderGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+  sumEl.textContent = "Sum: " + sum;
+
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+}
+
+function newCard() {
+  // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    cards.push(card);
+    sum += card;
+    renderGame();
+  }
+}
